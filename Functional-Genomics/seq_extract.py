05f876144f7fccaf648147d7b319e37e859a4a14
Eli <97261650+ERBringHorvath@users.noreply.github.com>
2024-01-06 09:00:40 -0700

    Update seq_extract.py

--- Functional-Genomics/seq_extract.py
+++ Functional-Genomics/seq_extract.py
@@ -1,5 +1,6 @@
 from Bio import SeqIO
 
+##Function to extract sequencing matching search key
 def extract_sequences_seqio(input_file, output_file, genome_name):
     with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
         for record in SeqIO.parse(infile, "fasta"):
@@ -10,9 +11,10 @@ def extract_sequences_seqio(input_file, output_file, genome_name):
     
     return found
 
-input_file = '/Users/ebh/Desktop/GO/Filtered_019_MLST_Hits.fa'
-output_file = '/Users/ebh/Desktop/GO/GSL17_019_Hits.fa'
-genome_name = 'GSL17_019'
+##Define input and output files
+input_file = '/path/to/file'
+output_file = '/path/to/file'
+genome_name = 'search_key' ##Define seach key
 
 if not extract_sequences_seqio(input_file, output_file, genome_name):
     print(f"No sequences found for genome name {genome_name}")
